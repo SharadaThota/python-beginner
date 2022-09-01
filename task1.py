bb43cc5b981c0d662d8253e61c170d991adcb6da
SharadaThota <105933031+SharadaThota@users.noreply.github.com>
2022-09-01 22:47:46 +0530

    Add files via upload

--- task1.py
+++ task1.py
@@ -6,13 +6,26 @@ mydb = conn.connect(host = "localhost" , user = 'root', passwd ="Ammu#9118")
 cursor = mydb.cursor()
 cursor.execute("create database if not exists tasksql")
 cursor.execute("create table if not exists tasksql.mysqltable( name varchar(30),number int)")
-@app.route('/insert',methods=['POST'])
+@app.route('/insert', methods=['POST'])
 def insert():
     if request.method == 'POST':
         name = request.json['name']
         number = request.json['number']
         cursor.execute("insert into tasksql.mysqltable values(%s,%s)",(name,number))
         mydb.commit()
-        return jsonify(str('successfully inserted'))
+        return jsonify(str("successfully inserted"))
+
+@app.route('/update', methods =['POST'])
+def update():
+    if request.method == 'POST':
+            get_name = request.json['get_name']
+            cursor.execute("update tasksql.mysqltable  set number = number - 4 where name = %s", (get_name,))
+            mydb.update()
+            return jsonify(str("successfully updated"))
+
+
 if __name__=='__main__':
     app.run()
+
+
+
